cbf437fa35c18900896f5dfea4919db6a51f545a
ZarinaAttaria <146745823+ZarinaAttaria@users.noreply.github.com>
2024-09-15 14:14:33 +0500

    contact page styling

--- frontend/src/Contact.jsx
+++ frontend/src/Contact.jsx
@@ -5,7 +5,9 @@ import {
   BiLogoLinkedinSquare,
   BiMailSend,
   BiPhoneCall,
+  BiSend,
   BiSupport,
+  BiUser,
 } from "react-icons/bi";
 
 import "./Contact.css";
@@ -13,20 +15,56 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <div className="allPage">
-      <div className="row contactus">
-        <div className="col-md-4">
-          <div className="contactcard">
-            <h1 className="contactus-title p-2  text-center">CONTACT ME</h1>
-            {/* <p className="text-justify mt-2">
-              If you have any queries or need more information about my
-              projects.
-            </p> */}
-            <p className="mt-3">
-              <BiMailSend className="icon2" /> : zarinaattaria135@gmail.com
-            </p>
-            <p className="mt-3">
-              <BiPhoneCall className="icon2" /> : 012-3456789
-            </p>
+      <div className="contact-heading">
+        <img
+          className="about-image"
+          src="https://img.icons8.com/?size=100&id=CCDYqb5KK5vI&format=png&color=000000"
+        />
+        <h1 className="contactus-title">Get In </h1>
+        <h1 className="contactus-title touch">Touch</h1>
+      </div>
+
+      <div className=" contactus">
+        <div className="contactcard">
+          <img src="contact.png" className="contact-image" />
+          <div>
+            <form className="contact-form">
+              <label>
+                <div className="input-container">
+                  <BiUser className="input-icon" />
+                  <input type="text" placeholder="Name" />
+                </div>
+              </label>
+              <label>
+                <div className="input-container">
+                  <BiMailSend className="input-icon" />
+                  <input type="email" placeholder="Email" />
+                </div>
+              </label>
+              <label>
+                <div className="input-container">
+                  <BiPhoneCall className="input-icon" />
+                  <input type="text" placeholder="Phone" />
+                </div>
+              </label>
+              <label>
+                <div className="input-container">
+                  <BiSupport className="input-icon" />
+                  <input
+                    type="text"
+                    placeholder="Message"
+                    className="messageInput"
+                  />
+                </div>
+              </label>
+              <div className="submit-container">
+                <input
+                  type="submit"
+                  placeholder="Submit"
+                  className="submitBtn"
+                />
+              </div>
+            </form>
           </div>
         </div>
       </div>
